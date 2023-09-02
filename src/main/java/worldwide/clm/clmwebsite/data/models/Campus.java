7fe370841f8e86fcfe77e8a01be19a11368564b2
Sam Abiodun <samuelabiodun324@gmail.com>
2023-09-02 08:08:17 +0100

    campus management update

--- src/main/java/worldwide/clm/clmwebsite/data/models/Campus.java
+++ src/main/java/worldwide/clm/clmwebsite/data/models/Campus.java
@@ -1,5 +1,6 @@
 package worldwide.clm.clmwebsite.data.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -9,6 +10,7 @@ import lombok.*;
 @RequiredArgsConstructor
 @Builder
 @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Campus {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
